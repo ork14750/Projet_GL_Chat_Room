5bf9da2833ea16f59168e43f9513e73af8cb0a0e
christophekede <chris.kede@yahoo.fr>
2020-05-25 21:45:56 +0200

    :sparkles: add MessageType test

--- src/server/Tests.java
+++ src/server/Tests.java
@@ -1,14 +1,16 @@
 package server;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 class Tests {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void MessageType() {
+		Message message = new Message("TEST", "Chris", "salut", "Jean Bapt");
+
+		assertEquals("{type='TEST', sender='Chris', body='salut', recipient='Jean Bapt'}", message.toString());
 	}
 
 }
